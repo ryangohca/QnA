79c1bb63923d61288a6c7f17c99aba3c71bbac57
Bryan Wu <35761373+deadeye888@users.noreply.github.com>
2021-08-17 23:51:39 +0800

    Update views.py

--- QnA/views.py
+++ QnA/views.py
@@ -1,11 +1,12 @@
 from QnA import app
 from flask import render_template, request, flash, url_for, redirect
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def root():
+    if (request.method == "POST"):
+        return "OK", 200
     return render_template("main.html")
 
 # @app.route("/edit")
 # def editor():
 #     return render_template("editor.html")
-
