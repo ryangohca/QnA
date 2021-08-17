2d8a4ae9cec9bc89c53b8ffd9ea9ebc513a6c95d
Ryan Goh <56359753+ryangohca@users.noreply.github.com>
2021-08-17 08:49:08 +0000

    include image

--- QnA/static/script.js
+++ QnA/static/script.js
@@ -3,6 +3,7 @@ let startPos;
 var drawing = false;
 let currFocusedCanvas;
 var objects = {};
+var baseImage = {};
 
 function drawRect(id, topx, topy, botx, boty){
     var c = document.getElementById(id);
@@ -14,7 +15,9 @@ function drawRect(id, topx, topy, botx, boty){
 
     ctx.rect(topx, topy, Math.max(1, botx-topx), Math.max(1, boty-topy));
     ctx.fillStyle = "#33CCCC";
+    ctx.globalAlpha = 0.2;
     ctx.fillRect(topx, topy, Math.max(1, botx-topx), Math.max(1, boty-topy));
+    ctx.globalAlpha = 1;
     ctx.stroke();
 
 }
@@ -28,7 +31,9 @@ function getMousePos(evt, canvas) {
 }
 
 function redraw(canvas){
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(baseImage[canvas.id], 0, 0);
     for (var prevRects of objects[canvas.id]){
         drawRect(canvas.id, prevRects.startX, prevRects.startY, prevRects.endX, prevRects.endY);
     }
@@ -71,16 +76,30 @@ function switch_mode(evt) {
     console.log(evt.code);
 }
 
+function setBaseImage(src, canvas) {
+    var img = new Image();
+    img.src = src;
+    img.onload = function() {
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        baseImage[canvas.id] = img;
+        canvas.width = img.width;
+        canvas.height = img.height;
+    }
+    
+}
+
 window.onload = function(){
     //drawRect("picture", 0, 0, 100, 100);
     for (var canvas of document.getElementsByClassName("drawRectCanvas")){
         // canvas.addEventListener('mousedown', function(){drawRect(canvas.id, 0,0, 100, 100)});
         objects[canvas.id] = [];
+        setBaseImage('static/sample.png', canvas);
         canvas.addEventListener("mousedown", function(e){handle_click(e, canvas)});
         canvas.addEventListener("mousemove", function(e){handle_move(e, canvas)});
         canvas.addEventListener("mouseup", function(e){handle_release(e, canvas)});
     }
-    canvas.addEventListener("keydown", function(e){switch_mode(e)});
+    canvas.addEventListener("keydown", switch_mode);
 }
 
 window.addEventListener('mousemove', function(e){
