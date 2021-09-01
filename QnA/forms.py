3f786292a0c42eee740fe2c27d4de1556247e4f6
Glitch (newqna) <none>
2021-09-01 14:53:35 +0000

    Added some validation for login / signup forms (untested)

--- QnA/forms.py
+++ QnA/forms.py
@@ -1,16 +1,55 @@
+import string
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
 
-from wtforms.validators import InputRequired, ValidationError, DataRequired, EqualTo
+from QnA.models import Users
 
+def userExists(form, field):
+    username = field.data
+    return len(Users.query.filter_by(username=username).all()) != 0
+  
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     
+    def validate_username(form, field):
+        if not userExists(form, field):
+            raise ValidationError("Username does not exist in server.")
+            
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(6, -1, "Password must be at least 6 characters long.")])
     confirm = PasswordField('Confirm Password', validators=[
         EqualTo("password", "Passwords do not match!")
     ])
+    
+    def validate_username(form, field):
+        if userExists(form, field):
+            raise ValidationError("Username already exists in server.")
+            
+    def validate_password(form, field):
+        validated = True
+        
+        password = field.data
+        if not any(char in set(string.punctuation + string.digits) for char in password):
+            form.password.errors.append("Password must have at least 1 special character (punctuation or number).")
+            validated = False
+            
+        if not any(char in string.ascii_uppercase for char in password):
+            form.password.errors.append("Password must have at least 1 uppercase character.")
+            validated = False
+            
+        if not any(char in string.ascii_lowercase for char in password):
+            form.password.errors.append("Password must have at least 1 lowercase character.")
+            validated = False 
+        
+        if form.username in password:
+            form.name.errors.append("Password must not contain username.")
+            validated = False
+            
+        return validated
+            
+        
