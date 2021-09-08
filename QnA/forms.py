22129590b8344cfbddd26228d1f76072aa3cd957
Glitch (newqna) <none>
2021-09-08 02:15:20 +0000

    added ability to save info to db for Questions

--- QnA/forms.py
+++ QnA/forms.py
@@ -3,10 +3,10 @@ import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, HiddenField, SelectField, IntegerField
-from wtforms.validators import ValidationError, InputRequired, EqualTo, Length, NumberRange
+from wtforms.validators import ValidationError, InputRequired, EqualTo, Length, NumberRange, Optional
 from flask_login import current_user
 
-from QnA.models import Users, DocumentUploads
+from QnA.models import Users, DocumentUploads, Answers, Questions
 
 def userExists(form, field):
     username = field.data.strip()
@@ -82,13 +82,14 @@ class SignupForm(FlaskForm):
             
 class TagForm(FlaskForm):
     imageType = SelectField('Type*:', id="tag-imageType", choices=[('question', 'Question Statement'), ('answer', 'Answer')], default='question', validators=[InputRequired()])
-    subject = StringField('Subject*:', id="tag-title")
-    topic = StringField('Topic*:', id="tag-topic")
-    year = IntegerField('Year:', id='tag-year', validators=[NumberRange(min=1950, max=datetime.datetime.now().year, message='Invalid year: please enter a year between %(min)s and %(max)s.')])
-    paper = StringField('Paper:', id='tag-paper')
-    questionNo = IntegerField('Question Number*:', id="tag-qnNo", validators=[InputRequired(), NumberRange(min=1, message='Question Number must be positive!')])
+    subject = StringField('Subject:', id="tag-title")
+    topic = StringField('Topic:', id="tag-topic")
+    year = IntegerField('Year:', id='tag-year', validators=[Optional(), NumberRange(min=1950, max=datetime.datetime.now().year, message='Invalid year: please enter a year between %(min)s and %(max)s.')])
+    paper = StringField('Paper Title (please be as specific as possible):', id='tag-paper')
+    questionNo = IntegerField('Question Number:', id="tag-qnNo", validators=[Optional(), NumberRange(min=1, message='Question Number must be positive!')])
     questionPart = StringField('Part:', id="tag-qnPart")
     questionDocument = SelectField('Question From:', id="tag-questionDoc", coerce=int, choices=[], default="")
+    paperSelect = SelectField('Choose paper:', id="tag-qnPaper") #This field will be populated with Javascript
     answer = StringField('Answer (leave blank if not in text):', id="tag-qnAns")
     
     def __init__(self, *args, **kwargs):
@@ -97,13 +98,28 @@ class TagForm(FlaskForm):
         super().__init__(*args, **kwargs)
         self.questionDocument.choices = getAllUploadDocuments(current_user.id)
         self.questionDocument.default = kwargs['documentID']
-        
-    def validate_topic(form, field):
-        if form.imageType.data == "question" and not field.data.strip():
-            raise ValidationError("Topic is required.")
-        return True
-      
-    def validate_subject(form, field):
-        if form.imageType.data == "question" and not field.data.strip():
-            raise ValidationError("Subject is required.")
-        return True
+
+    def validate_questionPart(form, field):
+        # Firstly, reject any forms with a question part but no question number.
+        if field.data is None and form.questionNo.data is not None:
+            raise ValidationError("All question part must follow a valid question number.")
+        # If questionType is answer, we just need to check whether a corresponding qn exist.
+        # However, we prioritise entry of data into database first, so if any of (year, paper, questionNo)
+        # is missing, we just validate the form and throw whatever data inside.
+        # In the case of answers, the answer would just be stored but inaccessible to users if it is not linked
+        # to a qn.
+        if form.imageType.data == "answer":
+            # We would need to implement dropdown list for papers before we can do this.
+            return True
+        # If questionType is question, we just need to check whether the following condition is True:
+        #   There should be no 2 questions with the same (year, paper, questionNo, questionPart).
+        # Similarly, if data is not available for checking, we just throw data into database if possible.
+        # The only exception is when year is not specified but paper and questionNo is satisfied. We would prompt
+        # user to enter a valid year if there are potential clashes.
+        elif form.imageType.data == "question":
+            if form.paper.data is None or form.questionNo.data is None:
+                return True
+            year = form.year.data
+            if len(Questions.query.filter_by(year=year, paper=form.paper.data.strip(), questionNo=form.questionNo.data, questionPart=form.questionPart.data.strip()).all()) != 0:
+                raise ValidationError("This question already exists in server. Did you mistype `questionNo`?")
+            return True
