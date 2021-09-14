fe0934ec86decd5d4710c355a2de34a6e963772a
Ryan Goh <56359753+ryangohca@users.noreply.github.com>
2021-09-14 17:05:04 +0800

    testing main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 
 from QnA import app
 
+@app.route('/')
+def test():
+    return "hi!", 200
+ 
 # Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
