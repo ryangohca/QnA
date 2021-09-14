75a7144f42a3d2c2761b9aedd1792a8f06316ac2
Ryan Goh <56359753+ryangohca@users.noreply.github.com>
2021-09-14 17:17:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 from QnA import app
- 
+
+raise ValueError('hi')
 # Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
